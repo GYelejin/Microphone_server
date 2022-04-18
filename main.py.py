54c2662fb4b3312280d0f17067952cfeb3dbd6d3
GYelejin <gyelejin@gmail.com>
2022-04-18 09:43:56 +0300

    nice try

---
+++ main.py.py
@@ -0,0 +1,43 @@
+import socket
+import logging
+
+
+logging.basicConfig(filename="server.log",
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)s:%(message)s')
+
+BUFFSIZE = config["BUFFSIZE"]
+
+with open("appsettings.json", "r") as read_file:
+    config = json.load(read_file)
+
+
+def printlog(message):
+    print(message)
+    logging.info(message)
+
+
+def handle_connection(conn):
+    filename = conn.recv(BUFFSIZE).decode("utf8")
+    file = open("sent_"+filename, "ab")
+    while True:
+        data = conn.recv(BUFFSIZE)
+        file.write(data)
+        if not data:
+            print("Done, file saved as {}".format("sent_"+filename))
+            conn.close()
+            file.close()
+            break
+
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((config["HOST"], config["PORT"]))
+    server.listen()
+    connection, address = server.accept()
+    printlog("Connected")
+    handle_connection(connection)
+
+
+if __name__ == "__main__":
+    main()
